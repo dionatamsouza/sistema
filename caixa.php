944aca062e3c981a45ccf18e211fc44a0e5e6192
Ryan Viana <contato.ryanviana@gmail.com>
2020-03-06 16:39:21 -0300

    Correção no codigo

--- caixa.php
+++ caixa.php
@@ -17,50 +17,56 @@
         	<div class="d-sm-flex align-items-center justify-content-between mb-4">
             <h1 class="h3 mb-0 text-gray-800">Caixa atual</h1>
           </div>
-          <div class="card shadow mb-4">
-            <div class="card-header py-3">
-              <h6 class="m-0 font-weight-bold text-primary">Escolha um período para ver o relatório:</h6>
-            </div>
-            <div class="card-body">
-              <form action="caixa.php" method="POST">
-                <div class="form-group row">
-                  <div class="col-sm-6">  
-                    <input type="text" class="form-control form-control-user" id="datainicio" name="datainicio" placeholder="Início" autocomplete="off" >
-                  </div>
-                  <div class="col-sm-6"> 
-                    <input type="text" class="form-control form-control-user" id="datafinal" name="datafinal" placeholder="Final" autocomplete="off" >
-                    <script type="text/javascript">
-                      $('#datainicio').datepicker({ 
-                        format: "dd/mm/yyyy", 
-                        language: "pt-BR",
-                        startDate: '-38600d',
-                      });
-                      $('#datafinal').datepicker({  
-                        format: "dd/mm/yyyy", 
-                        language: "pt-BR",
-                        startDate: '-38600d',
-                      });
-                    </script>
-                  </div>
-                </div>
-                <input type="submit" name="cadastrar-conta" class="btn btn-primary btn-user btn-block" value="Ver Relatório">
-              </form>
-            </div>
-          </div>
           <div class="row">
-            <table class="table table-striped">
-              <thead>
-                <tr>
-                  <th scope="col">Data</th>
-                  <th scope="col">Valor</th>
-                  <th scope="col">Quem</th>
-                  <th scope="col">Tipo</th>
-                </tr>
-              </thead>
-              <tbody>
-                <?php listarcaixa(); ?>   
-              </tbody>
-            </table>
+            <div class="col-lg-4">
+              <div class="card shadow mb-4">
+                <div class="card-header py-3">
+                  <h6 class="m-0 font-weight-bold text-primary">Escolha um período para ver o relatório:</h6>
+                </div>
+                <div class="card-body">
+                  <form action="caixa.php" method="POST">
+                    <div class="form-group row">
+                      <div class="col-sm-6">  
+                        <input type="text" class="form-control form-control-user" id="datainicio" name="datainicio" placeholder="Início" autocomplete="off" >
+                      </div>
+                      <div class="col-sm-6"> 
+                        <input type="text" class="form-control form-control-user" id="datafinal" name="datafinal" placeholder="Final" autocomplete="off" >
+                        <script type="text/javascript">
+                          $('#datainicio').datepicker({ 
+                            format: "dd/mm/yyyy", 
+                            language: "pt-BR",
+                            startDate: '-38600d',
+                          });
+                          $('#datafinal').datepicker({  
+                            format: "dd/mm/yyyy", 
+                            language: "pt-BR",
+                            startDate: '-38600d',
+                          });
+                        </script>
+                      </div>
+                    </div>
+                    <input type="submit" name="cadastrar-conta" class="btn btn-primary btn-user btn-block" value="Ver Relatório">
+                  </form>
+                </div>
+              </div>
+            </div>
+            <div class="row">
+              <div class="col-lg-12">
+                <table class="table table-striped">
+                  <thead>
+                    <tr>
+                      <th scope="col">Data</th>
+                      <th scope="col">Valor</th>
+                      <th scope="col">Quem</th>
+                      <th scope="col">Tipo</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <?php listarcaixa(); ?>   
+                  </tbody>
+                </table>
+              </div>
+            </div>
           </div>
         </div>
       <?php include_once("includes/footer.php"); ?>
