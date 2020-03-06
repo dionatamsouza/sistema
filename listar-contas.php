b6bcf419443391900c84f445efe8582e6468b874
Ryan Viana <contato.ryanviana@gmail.com>
2020-03-06 10:39:15 -0300

    Ajustes

--- listar-contas.php
+++ listar-contas.php
@@ -1,11 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
-<script>
-  function lalau() {
-    alert('teste');
-  }
-</script>
-<?php include_once("funcoes.php"); ?>
+<?php include_once("includes/head.php"); ?>
+<?php include_once("includes/funcoes.php"); ?>
 <body id="page-top">
   <div id="wrapper">
     <?php include_once("includes/sidebar.php"); ?>
@@ -14,22 +10,22 @@
         <?php include_once("includes/topbar.php"); ?>
         <div class="container-fluid">
         	<div class="d-sm-flex align-items-center justify-content-between mb-4">
-            	<h1 class="h3 mb-0 text-gray-800">Contas a pagar e a Receber</h1>
-            	</div>
-            	<script>
-            		function tipodeconta(){
-            			var e = document.getElementById("tipo");
-                  var selecionado = e.options[e.selectedIndex].value;
-            			window.location = "listar-contas.php?tipo="+selecionado;
-                }
-            	</script>
-            	<div class="d-sm-flex align-items-center justify-content-between mb-5">
-            	<h3 class="h5 mb-0 text-gray-800">Selecine o tipo de conta</h3>
-            	<select class="form-control form-control-user select" id="tipo" name="tipo" onchange='tipodeconta()' >
-                <option value="0">Tipo de conta</option>
-                <option value="1" onclick="teste()">Conta a Receber</option>
-                <option value="2">Conta a Pagar</option>
-					    </select>
+            <h1 class="h3 mb-0 text-gray-800">Contas a pagar e a Receber</h1>
+          </div>
+        	<script>
+        		function tipodeconta(){
+        			var e = document.getElementById("tipo");
+              var selecionado = e.options[e.selectedIndex].value;
+        			window.location = "listar-contas.php?tipo="+selecionado;
+            }
+        	</script>
+        	<div class="d-sm-flex align-items-center justify-content-between mb-5">
+          	<h3 class="h5 mb-0 text-gray-800">Selecine o tipo de conta</h3>
+          	<select class="form-control form-control-user select" id="tipo" name="tipo" onchange='tipodeconta()' >
+              <option value="0">Tipo de conta</option>
+              <option value="1" onclick="teste()">Conta a Receber</option>
+              <option value="2">Conta a Pagar</option>
+  			    </select>
         	</div>
         	<table class="table table-striped">
             <thead>
@@ -38,7 +34,7 @@
                 <th scope="col">Valor</th>
                 <th scope="col">Quem</th>
                 <th scope="col">Tipo</th>
-                 <th scope="col">Concluir</th>
+                <th scope="col">Concluir</th>
               </tr>
             </thead>
             <tbody>
