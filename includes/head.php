defcefd29ad4d6e60b4fb965f232a402ddceeef1
Ryan Viana <contato.ryanviana@gmail.com>
2020-03-06 16:47:23 -0300

    Ajustes

--- includes/head.php
+++ includes/head.php
@@ -1,15 +1,13 @@
 <?php include_once("includes/bdd.php"); ?>
 <head>
 	<meta charset="utf-8">
-  	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-  	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  	<meta name="description" content="">
-  	<meta name="author" content="">
-  	<title><?php echo $gerenciador_nome; ?></title>
-  	<link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
-  	<link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
-  	<link href="css/sb-admin-2.css" rel="stylesheet">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<meta name="description" content="">
+	<meta name="author" content="">
+	<title><?php echo $gerenciador_nome; ?></title>
+	<link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+	<link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
+  <link href="css/sb-admin-2.css" rel="stylesheet">
 	<link href="calendario/css/bootstrap-datepicker.css" rel="stylesheet"/>
-	<script src="calendario/js/bootstrap-datepicker.min.js"></script> 
-	<script src="calendario/js/bootstrap-datepicker.pt-BR.min.js" charset="UTF-8"></script>
 </head>
