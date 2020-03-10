b93a5452e802b3634cd7dd459627dc99981bde33
Ryan Viana <contato.ryanviana@gmail.com>
2020-03-10 14:36:16 -0300

    Ajustes

--- includes/imoveis.php
+++ includes/imoveis.php
@@ -12,6 +12,13 @@
             $omsid = $omsmost->id;
             $omstitulo = $omsmost->titulo;
             $omsdescricao = $omsmost->descricao;
+            $omstipo = $omsmost->tipo;
+            $omstipodepropriedade = $omsmost->tipo_de_propriedade;
+            $omsendereco = $omsmost->endereco;
+            $omsvalor = $omsmost->valor;
+            $omsnumerodedormitorios = $omsmost->numero_de_dormitorios;
+            $omsareaconstruida = $omsmost->area_construida;
+            $omsareaterrenototal = $omsmost->area_terreno_total;
             $omsfotoimovel = $omsmost->foto_imovel;
   ?>
             <div class="col-lg-9">
@@ -23,18 +30,14 @@
                 </div>
                 <div class="card-body">
                   <p><strong>Descrição:</strong> <?php echo $omsdescricao; ?></p>
-                  <div class="col-md-3 d-inline-flex">
-                    <p><i class="fas fa-bed"></i> 4</p>
-                  </div>
-                  <div class="col-md-3 d-inline-flex">
-                    <p><i class="fas fa-bath"></i> 4</p>
-                  </div>
-                  <div class="col-md-3 d-inline-flex">
-                    <p><i class="fas fa-swimming-pool"></i> 4</p>
-                  </div>
-                  <div class="col-md-3 d-inline-flex">
-                    <p><i class="fas fa-car"></i> 4</p>
-                  </div>
+                  <p><strong>ID do anúncio:</strong> <?php echo $omsid; ?></p>
+                  <p><strong>Tipo:</strong> <?php echo $omstipo; ?></p>
+                  <p><strong>Tipo de propriedade:</strong> <?php echo $omstipodepropriedade; ?></p>
+                  <p><strong>Endereço:</strong> <?php echo $omsendereco; ?></p>
+                  <p><strong>Valor:</strong> <?php echo $omsvalor; ?></p>
+                  <p><strong>Número de dormitórios:</strong> <?php echo $omsnumerodedormitorios; ?></p>
+                  <p><strong>Área construída:</strong> <?php echo $omsareaconstruida; ?></p>
+                  <p><strong>Área terreno/total:</strong> <?php echo $omsareaterrenototal; ?></p>
                 </div>
               </div>
             </div>
