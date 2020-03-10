e3fa35266cb5cbdd54ca2cc26aea0b86866a1e16
Ryan Viana <contato.ryanviana@gmail.com>
2020-03-10 14:53:22 -0300

    Correção

--- includes/imoveis.php
+++ includes/imoveis.php
@@ -63,8 +63,6 @@
                   <p><i class="fas fa-map-marker-alt"></i> <?php echo $omsendereco; ?></p>
                 </div>
               </div>
-            </div>
-            <div class="col-lg-3">
               <div class="card shadow mb-4">
                 <div class="card-header py-3">
                   <h6 class="m-0 font-weight-bold text-primary">Calculadora</h6>
