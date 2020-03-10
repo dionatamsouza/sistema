4fa38159517211d477feccd3301398a6b0583a1e
Ryan Viana <contato.ryanviana@gmail.com>
2020-03-10 14:00:39 -0300

    Teste

--- includes/imoveis.php
+++ includes/imoveis.php
@@ -1,10 +1,7 @@
 <?php
   $imoveis = $_GET['imoveis'];
   if(isset($imoveis) AND !empty($imoveis)) {
-    echo "teste";
-  }
-  else {
-    $omsselect = "SELECT * from imoveis WHERE tipo=$tipopage AND imobiliaria_creci=$lgnimobiliaria_creci";
+    $omsselect = "SELECT * from imoveis WHERE id=$imoveis AND imobiliaria_creci=$lgnimobiliaria_creci";
     try {
       $omsresult = $bdd->prepare($omsselect);
       $omsresult->execute();
@@ -25,7 +22,7 @@
                   <h6 class="m-0 font-weight-bold text-primary"><?php echo $omstitulo; ?></h6>
                 </div>
                 <div class="card-body">
-                  <p><strong>Descrição:</strong> <?php echo $omsdescricao; ?></p>
+                  <p><strong>aaaaDescrição:</strong> <?php echo $omsdescricao; ?></p>
                   <div class="col-md-3 d-inline-flex">
                     <p><i class="fas fa-bed"></i> 4</p>
                   </div>
@@ -46,11 +43,55 @@
         }
       }
       else {
-          echo '<div class="ml-md-3">Nada encontrado..</div>';
+        $omsselect = "SELECT * from imoveis WHERE tipo=$tipopage AND imobiliaria_creci=$lgnimobiliaria_creci";
+        try {
+          $omsresult = $bdd->prepare($omsselect);
+          $omsresult->execute();
+          $omscontar = $omsresult->rowCount();
+          if($omscontar>0) {
+            while($omsmost = $omsresult->FETCH(PDO::FETCH_OBJ)) {
+              $omsid = $omsmost->id;
+              $omstitulo = $omsmost->titulo;
+              $omsdescricao = $omsmost->descricao;
+              $omsfotoimovel = $omsmost->foto_imovel;
+    ?>
+              <div class="col-lg-3">
+                <a href="?imoveis=<?php echo $omsid; ?>">
+                  <div class="card shadow mb-4">
+                    <a class="btn-trash" href="aaaa"><i class="fas fa-times"></i></a>
+                    <img src="<?php echo $omsfotoimovel; ?>" width="100%">
+                    <div class="card-header py-3">
+                      <h6 class="m-0 font-weight-bold text-primary"><?php echo $omstitulo; ?></h6>
+                    </div>
+                    <div class="card-body">
+                      <p><strong>Descrição:</strong> <?php echo $omsdescricao; ?></p>
+                      <div class="col-md-3 d-inline-flex">
+                        <p><i class="fas fa-bed"></i> 4</p>
+                      </div>
+                      <div class="col-md-3 d-inline-flex">
+                        <p><i class="fas fa-bath"></i> 4</p>
+                      </div>
+                      <div class="col-md-3 d-inline-flex">
+                        <p><i class="fas fa-swimming-pool"></i> 4</p>
+                      </div>
+                      <div class="col-md-3 d-inline-flex">
+                        <p><i class="fas fa-car"></i> 4</p>
+                      </div>
+                    </div>
+                  </div>
+                </a>
+              </div>
+    <?php
+            }
+          }
+          else {
+              echo '<div class="ml-md-3">Nada encontrado..</div>';
+          }
+        }
+        catch(PDOException $e) {
+          echo $e;
+        }
       }
-    }
-    catch(PDOException $e) {
-      echo $e;
     }
   }
 ?>
