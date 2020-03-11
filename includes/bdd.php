8e08fd1aaf943babe27ed441447dbbdade7d4d2b
Ryan Viana <contato.ryanviana@gmail.com>
2020-03-11 09:12:46 -0300

    Correções

--- includes/bdd.php
+++ includes/bdd.php
@@ -1,6 +1,16 @@
 <?php
   //conexão ao banco de dados
-  include_once("conexao.php");
+  ob_start();
+  session_start();
+  date_default_timezone_set('America/Sao_Paulo');
+  error_reporting(E_ALL & ~E_NOTICE & ~E_WARNING);
+  try {
+    $bdd = new PDO('mysql:host=localhost;dbname=u831509106_gnr;charset=utf8', 'u831509106_gnr', 'gerenciador2020');
+    $bdd -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  }
+  catch(PDOException $error) {
+    echo 'Error: ' . $error -> getMessage();
+  }
   //-----------------------------------------------------------------------------------
 
   //configurações do site
