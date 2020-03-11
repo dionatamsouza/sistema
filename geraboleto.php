584a7c891897f0ab80f0dabc2c67f2100a429451
netsites-code <61599701+netsites-code@users.noreply.github.com>
2020-03-11 07:24:52 -0300

    Update geraboleto.php

--- geraboleto.php
+++ geraboleto.php
@@ -1,10 +1,10 @@
-<?php //include_once("header.php");
+<?php include_once("includes/head.php");
 $valor=$_POST['valor'];
 $clientepulsar=$_POST['cliente'];
 $vencimento=$_POST['data'];
+$opcao=$_POST['opcaobleto'];
 $vencimentop = implode('/', array_reverse(explode('/', $vencimento)));
-$url="http://ocv.net.br/gi/geraboleto-ocv.php?valor=&cliente=$clientepulsar&data=$vencimentop&valor=$valor";
+$url="http://ocv.net.br/gi/geraboleto-ocv.php?valor=&cliente=$clientepulsar&data=$vencimentop&valor=$valor&opcaoboleto=$opcao";
 $result=file_get_contents($url);
 echo $result;
-
 ?>
