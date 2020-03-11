0b84b7e49d5c80ad0a2927950c9b722dda663b25
netsites-code <61599701+netsites-code@users.noreply.github.com>
2020-03-11 13:06:29 -0300

    alterações

--- funcoes.php
+++ funcoes.php
@@ -425,6 +425,244 @@ catch(PDOException $e) {
 		
 }
  
+ 
+ function base_boleto(){
+ 	$codigobanco = "001";
+$codigo_banco_com_dv = geraCodigoBanco($codigobanco);
+$nummoeda = "9";
+$fator_vencimento = fator_vencimento($dadosboleto["data_vencimento"]);
+
+
+$valor = formata_numero($dadosboleto["valor_boleto"],10,0,"valor");
+
+$agencia = formata_numero($dadosboleto["agencia"],4,0);
+
+$conta = formata_numero($dadosboleto["conta"],8,0);
+
+$carteira = $dadosboleto["carteira"];
+
+$agencia_codigo = $agencia."-". modulo_11($agencia) ." / ". $conta ."-". modulo_11($conta);
+$livre_zeros='000000';
+
+
+if ($dadosboleto["formatacao_convenio"] == "8") {
+	$convenio = formata_numero($dadosboleto["convenio"],8,0,"convenio");
+	
+	$nossonumero = formata_numero($dadosboleto["nosso_numero"],9,0);
+	$dv=modulo_11("$codigobanco$nummoeda$fator_vencimento$valor$livre_zeros$convenio$nossonumero$carteira");
+	$linha="$codigobanco$nummoeda$dv$fator_vencimento$valor$livre_zeros$convenio$nossonumero$carteira";
+	
+	$nossonumero = $convenio . $nossonumero ."-". modulo_11($convenio.$nossonumero);
+}
+
+if ($dadosboleto["formatacao_convenio"] == "7") {
+	$convenio = formata_numero($dadosboleto["convenio"],7,0,"convenio");
+	
+	$nossonumero = formata_numero($dadosboleto["nosso_numero"],10,0);
+	$dv=modulo_11("$codigobanco$nummoeda$fator_vencimento$valor$livre_zeros$convenio$nossonumero$carteira");
+	$linha="$codigobanco$nummoeda$dv$fator_vencimento$valor$livre_zeros$convenio$nossonumero$carteira";
+  $nossonumero = $convenio.$nossonumero;
+	
+}
+
+
+if ($dadosboleto["formatacao_convenio"] == "6") {
+	$convenio = formata_numero($dadosboleto["convenio"],6,0,"convenio");
+	
+	if ($dadosboleto["formatacao_nosso_numero"] == "1") {
+		
+		
+		$nossonumero = formata_numero($dadosboleto["nosso_numero"],5,0);
+		$dv = modulo_11("$codigobanco$nummoeda$fator_vencimento$valor$convenio$nossonumero$agencia$conta$carteira");
+		$linha = "$codigobanco$nummoeda$dv$fator_vencimento$valor$convenio$nossonumero$agencia$conta$carteira";
+	
+		$nossonumero = $convenio . $nossonumero ."-". modulo_11($convenio.$nossonumero);
+	}
+	
+	if ($dadosboleto["formatacao_nosso_numero"] == "2") {
+		
+		
+		$nservico = "21";
+		$nossonumero = formata_numero($dadosboleto["nosso_numero"],17,0);
+		$dv = modulo_11("$codigobanco$nummoeda$fator_vencimento$valor$convenio$nossonumero$nservico");
+		$linha = "$codigobanco$nummoeda$dv$fator_vencimento$valor$convenio$nossonumero$nservico";
+	}
+}
+
+$dadosboleto["codigo_barras"] = $linha;
+$dadosboleto["linha_digitavel"] = monta_linha_digitavel($linha);
+$dadosboleto["agencia_codigo"] = $agencia_codigo;
+$dadosboleto["nosso_numero"] = $nossonumero;
+$dadosboleto["codigo_banco_com_dv"] = $codigo_banco_com_dv;
+
+
+function formata_numero($numero,$loop,$insert,$tipo = "geral") {
+	if ($tipo == "geral") {
+		$numero = str_replace(",","",$numero);
+		while(strlen($numero)<$loop){
+			$numero = $insert . $numero;
+		}
+	}
+	if ($tipo == "valor") {
+	
+		$numero = str_replace(",","",$numero);
+		while(strlen($numero)<$loop){
+			$numero = $insert . $numero;
+		}
+	}
+	if ($tipo == "convenio") {
+		while(strlen($numero)<$loop){
+			$numero = $numero . $insert;
+		}
+	}
+	return $numero;
+}
+
+
+function fbarcode($valor){
+
+$fino = 1 ;
+$largo = 3 ;
+$altura = 50 ;
+
+  $barcodes[0] = "00110" ;
+  $barcodes[1] = "10001" ;
+  $barcodes[2] = "01001" ;
+  $barcodes[3] = "11000" ;
+  $barcodes[4] = "00101" ;
+  $barcodes[5] = "10100" ;
+  $barcodes[6] = "01100" ;
+  $barcodes[7] = "00011" ;
+  $barcodes[8] = "10010" ;
+  $barcodes[9] = "01010" ;
+  for($f1=9;$f1>=0;$f1--){ 
+    for($f2=9;$f2>=0;$f2--){  
+      $f = ($f1 * 10) + $f2 ;
+      $texto = "" ;
+      for($i=1;$i<6;$i++){ 
+        $texto .=  substr($barcodes[$f1],($i-1),1) . substr($barcodes[$f2],($i-1),1);
+      }
+      $barcodes[$f] = $texto;
+    }
+  }
+
+
+
+?><img src=imagens/p.png width=<?php echo $fino?> height=<?php echo $altura?> border=0><img 
+src=imagens/b.png width=<?php echo $fino?> height=<?php echo $altura?> border=0><img 
+src=imagens/p.png width=<?php echo $fino?> height=<?php echo $altura?> border=0><img 
+src=imagens/b.png width=<?php echo $fino?> height=<?php echo $altura?> border=0><img 
+<?php
+$texto = $valor ;
+if((strlen($texto) % 2) <> 0){
+	$texto = "0" . $texto;
+}
+
+// Draw dos dados
+while (strlen($texto) > 0) {
+  $i = round(esquerda($texto,2));
+  $texto = direita($texto,strlen($texto)-2);
+  $f = $barcodes[$i];
+  for($i=1;$i<11;$i+=2){
+    if (substr($f,($i-1),1) == "0") {
+      $f1 = $fino ;
+    }else{
+      $f1 = $largo ;
+    }
+?>
+    src=imagens/p.png width=<?php echo $f1?> height=<?php echo $altura?> border=0><img 
+<?php
+    if (substr($f,$i,1) == "0") {
+      $f2 = $fino ;
+    }else{
+      $f2 = $largo ;
+    }
+?>
+    src=imagens/b.png width=<?php echo $f2?> height=<?php echo $altura?> border=0><img 
+<?php
+  }
+}
+
+?>
+src=imagens/p.png width=<?php echo $largo?> height=<?php echo $altura?> border=0><img 
+src=imagens/b.png width=<?php echo $fino?> height=<?php echo $altura?> border=0><img 
+src=imagens/p.png width=<?php echo 1?> height=<?php echo $altura?> border=0> 
+  <?php
+} //Fim da função
+
+function esquerda($entra,$comp){
+	return substr($entra,0,$comp);
+}
+
+function direita($entra,$comp){
+	return substr($entra,strlen($entra)-$comp,$comp);
+}
+
+function fator_vencimento($data) {
+	$data = explode("/",$data);
+	$ano = $data[2];
+	$mes = $data[1];
+	$dia = $data[0];
+    return(abs((_dateToDays("1997","10","07")) - (_dateToDays($ano, $mes, $dia))));
+}
+
+function _dateToDays($year,$month,$day) {
+    $century = substr($year, 0, 2);
+    $year = substr($year, 2, 2);
+    if ($month > 2) {
+        $month -= 3;
+    } else {
+        $month += 9;
+        if ($year) {
+            $year--;
+        } else {
+            $year = 99;
+            $century --;
+        }
+    }
+
+    return ( floor((  146097 * $century)    /  4 ) +
+            floor(( 1461 * $year)        /  4 ) +
+            floor(( 153 * $month +  2) /  5 ) +
+                $day +  1721119);
+}
+
+/*
+#################################################
+
+#################################################
+*/
+function modulo_10($num) { 
+	$numtotal10 = 0;
+	$fator = 2;
+ 
+	for ($i = strlen($num); $i > 0; $i--) {
+		$numeros[$i] = substr($num,$i-1,1);
+		$parcial10[$i] = $numeros[$i] * $fator;
+		$numtotal10 .= $parcial10[$i];
+		if ($fator == 2) {
+			$fator = 1;
+		}
+		else {
+			$fator = 2; 
+		}
+	}
+	
+	$soma = 0;
+	for ($i = strlen($numtotal10); $i > 0; $i--) {
+		$numeros[$i] = substr($numtotal10,$i-1,1);
+		$soma += $numeros[$i]; 
+	}
+	$resto = $soma % 10;
+	$digito = 10 - $resto;
+	if ($resto == 0) {
+		$digito = 0;
+	}
+
+	return $digito;
+}
+
+ }
 
 
 ?>
--- geraboleto.php
+++ geraboleto.php
@@ -1,10 +1,14 @@
-<?php include_once("includes/head.php");
+<?php 
+include_once("includes/head.php");
 $valor=$_POST['valor'];
 $clientepulsar=$_POST['cliente'];
 $vencimento=$_POST['data'];
+$descricao=$_POST['descricao'];
 $opcao=$_POST['opcaobleto'];
 $vencimentop = implode('/', array_reverse(explode('/', $vencimento)));
-$url="http://ocv.net.br/gi/geraboleto-ocv.php?valor=&cliente=$clientepulsar&data=$vencimentop&valor=$valor&opcaoboleto=$opcao";
+$url="https://ocv.net.br/gi/geraboleto-ocv.php?valor=&cliente=$clientepulsar&data=$vencimentop&valor=$valor&opcaoboleto=$opcao&descricao=$descricao";
 $result=file_get_contents($url);
 echo $result;
+
+
 ?>
