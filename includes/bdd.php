1451896cf76135c2adb07f8daad5e069231c5db5
Ryan Viana <contato.ryanviana@gmail.com>
2020-03-12 13:58:03 -0300

    teste

--- includes/bdd.php
+++ includes/bdd.php
@@ -321,9 +321,15 @@
     $id_imovel = uniqid();
 
 
-    foreach ($_FILES['fotoimovel']['tmp_name'] as $chave=>$valor) {
+    $fotos= $_FILES['foto_imagens']['tmp_name'];
+
+
+    foreach ($fotos as $chave=>$valor) {
+      $hash=md5(uniqid(rand(), true));
+      $nome = "$hash";
+      copy($valor,"uploadteste/$nome.jpg");
       echo $valor;
-   	}
+    }
 
 
     /*
