c401e99d6b50034e1d3c74e560a2312db5d4e1bb
Ryan Viana <contato.ryanviana@gmail.com>
2020-03-12 14:08:24 -0300

    teste

--- includes/bdd.php
+++ includes/bdd.php
@@ -328,9 +328,9 @@
     $extensao = explode('.', $nome);
     $ext = end($extensao);
     $novonome = md5($nome).'.'.$ext;
-    
+
     for ($controle = 0; $controle < count($novonome); $controle++) {
-      $destino = $diretorio.$novonome[$controle];
+      $destino = $diretorio.$novonome.[$controle];
       if(move_uploaded_file($arquivo['tmp_name'][$controle], $destino)) {
         echo "Upload realizado com sucesso<br>";
       }
