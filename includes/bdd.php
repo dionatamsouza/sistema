2087104879acc6b1df2f7c17ed0b138f020475da
Ryan Viana <contato.ryanviana@gmail.com>
2020-03-12 14:04:03 -0300

    teste

--- includes/bdd.php
+++ includes/bdd.php
@@ -321,24 +321,16 @@
     $id_imovel = uniqid();
 
 
-    //diretório para salvar as imagens
-    $diretorio = "imgs/imoveis/";
-    //Verificar a existência do diretório para salvar as imagens e informa se o caminho é um diretório
-    if(!is_dir($diretorio)){ 
-        echo "Pasta $diretorio nao existe";
-    }else{    
-        $arquivo = isset($_FILES['fotoimovel']) ? $_FILES['fotoimovel'] : FALSE;
-        //loop para ler as imagens
-        for ($controle = 0; $controle < count($arquivo['name']); $controle++){        
-            $destino = $diretorio."/".$arquivo['name'][$controle];
-            //realizar o upload da imagem em php
-            //move_uploaded_file — Move um arquivo enviado para uma nova localização
-            if(move_uploaded_file($arquivo['tmp_name'][$controle], $destino)){
-                echo "Upload realizado com sucesso<br>"; 
-            }else{
-                echo "Erro ao realizar upload";
-            }        
-        }
+    $diretorio = "imgs/imoveis/";   
+    $arquivo = isset($_FILES['fotoimovel']) ? $_FILES['fotoimovel'] : FALSE;
+    for ($controle = 0; $controle < count($arquivo['name']); $controle++) {
+      $destino = $diretorio.$arquivo['name'][$controle];
+      if(move_uploaded_file($arquivo['tmp_name'][$controle], $destino)) {
+        echo "Upload realizado com sucesso<br>";
+      }
+      else {
+        echo "Erro ao realizar upload";
+      }
     }
 
 
