7ab21e813d01fb7514b87bb5e9351a091b90b9f8
Ryan Viana <contato.ryanviana@gmail.com>
2020-03-12 14:00:53 -0300

    teste

--- includes/bdd.php
+++ includes/bdd.php
@@ -321,14 +321,24 @@
     $id_imovel = uniqid();
 
 
-    $fotos= $_FILES['foto_imagens']['tmp_name'];
-
-
-    foreach ($fotos as $chave=>$valor) {
-      $hash=md5(uniqid(rand(), true));
-      $nome = "$hash";
-      copy($valor,"imgs/imoveis/$nome.jpg");
-      echo $valor;
+    //diretório para salvar as imagens
+    $diretorio = "imgs/imoveis/";
+    //Verificar a existência do diretório para salvar as imagens e informa se o caminho é um diretório
+    if(!is_dir($diretorio)){ 
+        echo "Pasta $diretorio nao existe";
+    }else{    
+        $arquivo = isset($_FILES['fotoimovel']) ? $_FILES['fotoimovel'] : FALSE;
+        //loop para ler as imagens
+        for ($controle = 0; $controle < count($arquivo['name']); $controle++){        
+            $destino = $diretorio."/".$arquivo['name'][$controle];
+            //realizar o upload da imagem em php
+            //move_uploaded_file — Move um arquivo enviado para uma nova localização
+            if(move_uploaded_file($arquivo['tmp_name'][$controle], $destino)){
+                echo "Upload realizado com sucesso<br>"; 
+            }else{
+                echo "Erro ao realizar upload";
+            }        
+        }
     }
 
 
