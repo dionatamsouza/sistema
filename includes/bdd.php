9d3f07bb7dfdfa0d1792c70618ef44c53a275994
Ryan Viana <contato.ryanviana@gmail.com>
2020-03-12 14:24:21 -0300

    teste

--- includes/bdd.php
+++ includes/bdd.php
@@ -319,10 +319,10 @@
   //cadastrar imóvel
   if(isset($_POST['cadastrar-imovel'])) {
     $id_imovel = uniqid();
-    $diretorio = "imgs/imoveis/";   
+    $diretorio = "/imgs/imoveis/";   
     $arquivo = isset($_FILES['fotoimovel']) ? $_FILES['fotoimovel'] : FALSE;
     for($controle = 0; $controle < count($arquivo['name']); $controle++) {
-      $destino = $diretorio.$arquivo['name'][$controle];
+      $destino = $diretorio.md5($arquivo['name'])[$controle];
       if(move_uploaded_file($arquivo['tmp_name'][$controle], $destino)) {
         $cadinsert = "INSERT into fotos_imovel (iddoimovel, file) VALUES (:iddoimovel, :file)";
         try {
