7eb7d2753c0a00b213b8d77c09d7279dd7e36f79
Ryan Viana <contato.ryanviana@gmail.com>
2020-03-13 10:56:38 -0300

    Voltei

--- includes/listar-contratos.php
+++ includes/listar-contratos.php
@@ -18,7 +18,7 @@
                 <div class="row no-gutters align-items-center">
                   <div class="col mr-2">
                     <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">Contrato</div>
-                    <div class="h5 mb-0 font-weight-bold text-gray-800">Modelo <?php echo count($omsresult); ?></div>
+                    <div class="h5 mb-0 font-weight-bold text-gray-800">Modelo 01</div>
                   </div>
                   <div class="col-auto">
                     <i class="fas fa-file-signature fa-2x text-gray-300"></i>
