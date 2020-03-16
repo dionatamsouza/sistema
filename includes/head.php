277d69cd02e6e558466a75ef756763098c6ca6a5
Ryan Viana <contato.ryanviana@gmail.com>
2020-03-16 09:28:31 -0300

    Update upload

--- includes/head.php
+++ includes/head.php
@@ -18,4 +18,23 @@
 	<link rel="stylesheet" href="css/responsiveslides.css">
 	<script src="js/responsiveslides.min.js"></script>
   	<script src="js/responsiveslides.js"></script>
+  	<link href="https://fonts.googleapis.com/css?family=Roboto:400,700" rel="stylesheet">
+    <link href="font/font-fileuploader.css" rel="stylesheet">
+	<link href="css/jquery.fileuploader.min.css" media="all" rel="stylesheet">
+	<script src="js/jquery.fileuploader.min.js" type="text/javascript"></script>
+	<script src="js/custom.js" type="text/javascript"></script>
+	<style>
+		body {
+			font-family: 'Roboto', sans-serif;
+			font-size: 14px;
+            line-height: normal;
+			background-color: #fff;
+
+			margin: 0;
+			padding: 20px;
+		}
+        .fileuploader {
+            max-width: 560px;
+        }
+	</style>
 </head>
