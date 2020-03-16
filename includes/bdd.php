028570d0c25c64e6be89555775a80beb8b568502
Ryan Viana <contato.ryanviana@gmail.com>
2020-03-16 09:46:37 -0300

    Revert "correção"
    This reverts commit 003f1274dc77b49c2b7248081761f507d2cf7b04.

--- includes/bdd.php
+++ includes/bdd.php
@@ -5,7 +5,7 @@
   date_default_timezone_set('America/Sao_Paulo');
   error_reporting(E_ALL & ~E_NOTICE & ~E_WARNING);
   try {
-    $bdd = new PDO('mysql:host=localhost;dbname=u831509106_gnr;charset=utf8', 'root', '');
+    $bdd = new PDO('mysql:host=localhost;dbname=u831509106_gnr;charset=utf8', 'u831509106_gnr', 'gerenciador2020');
     $bdd -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
   }
   catch(PDOException $error) {
